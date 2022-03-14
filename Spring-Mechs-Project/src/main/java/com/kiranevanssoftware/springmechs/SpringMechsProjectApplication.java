0a1a62ba1a17658b3de071166d8f161d0c61d12a
kiran-evans <thekiranevans@gmail.com>
2022-03-14 16:45:57 +0000

    KE: Setup Spring project. Added README.md and Risk Assessment.

---
+++ Spring-Mechs-Project/src/main/java/com/kiranevanssoftware/springmechs/SpringMechsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kiranevanssoftware.springmechs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMechsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMechsProjectApplication.class, args);
+	}
+
+}
